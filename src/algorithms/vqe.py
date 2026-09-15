e3584409981a82658f1215a08c9a896ea74dd077
GiveMeACent <anto.coltry@gmail.com>
2026-09-15 16:26:04 +0200

    Implement VQE class with initial structure and placeholder methods

---
+++ src/algorithms/vqe.py
@@ -0,0 +1,23 @@
+class VQE:
+  _hamiltonian = None
+  _initial_state = None
+  _ansatz = None
+  _parameters = None
+  _optimizer = None
+  _max_iterations: int = 0
+  _tolerance: float = 0
+
+  def __init__(self):
+    return
+
+  def prepare_circuit(self):
+    return
+
+  def evaluate_energy(self):
+    return
+
+  def optimize(self):
+    return
+
+  def run(self):
+    return
