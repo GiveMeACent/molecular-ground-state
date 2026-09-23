8ad477fb7e243e98bcf20286a7f73fd6bfb911d3
GiveMeACent <anto.coltry@gmail.com>
2026-09-23 14:59:18 +0200

    Fix UCJ methods, add simple test in main.

--- src/ansatze/ucj.py
+++ src/ansatze/ucj.py
@@ -6,6 +6,8 @@ from qiskit.circuit import Parameter
 from qiskit.circuit.library import XXPlusYYGate, PhaseGate, CPhaseGate
 import numpy as np
 import ffsim
+import cmath
+import math
 
 
 class SpinType(Enum):
@@ -15,10 +17,15 @@ class SpinType(Enum):
 
 
 class UCJ:
-  def __init__(self, num_spatial_orbitals: int, n_reps: int, spin_type: SpinType = SpinType.SpinBalanced):
+  def __init__(
+      self,
+      num_spatial_orbitals: int,
+      n_reps: int,
+      spin_type: SpinType = SpinType.SpinBalanced,
+  ):
     if spin_type is not SpinType.SpinBalanced:
       raise NotImplementedError(
-          f"{spin_type} is not implemented, only SpinBalanced is implemented for now."
+          f"{spin_type} is not implemented, only SpinBalanced is supported."
       )
     self._norb = num_spatial_orbitals
     self._n_reps = n_reps
@@ -29,91 +36,127 @@ class UCJ:
     self._orbital_rotations: np.ndarray | None = None
     self._final_orbital_rotation: np.ndarray | None = None
 
-    self._orbital_givens_indices_per_layer: list[list[tuple[int, int, float, float, Parameter, Parameter]]] = [
+    self._orbital_givens_per_layer: list[list[tuple]] = [
         [] for _ in range(self._n_reps + 1)
     ]
-    self._diagonal_phases_indices_per_layer: list[list[tuple[int, float, Parameter]]] = [
+    self._diagonal_phases_per_layer: list[list[tuple]] = [
         [] for _ in range(self._n_reps + 1)
     ]
-    self._jastrow_indices_per_layer: list[list[tuple[int, int, float, Parameter]]] = [
-        [] for _ in range(self._n_reps)
-    ]
+    self._jastrow_aa_per_layer: list[list[tuple]] = [
+        [] for _ in range(self._n_reps)]
+    self._jastrow_ab_per_layer: list[list[tuple]] = [
+        [] for _ in range(self._n_reps)]
+
     self._parameters: dict[Parameter, float] = {}
 
   def from_t_amplitudes(self, t1: np.ndarray, t2: np.ndarray):
-    self._diag_coulomb_mats, self._orbital_rotations = ffsim.linalg.double_factorized_t2(
-        t2, max_terms=self._n_reps)
-    self._final_orbital_rotation = ffsim.variational.util.orbital_rotation_from_t1_amplitudes(
-        t1)
+    ucj_op = ffsim.UCJOpSpinBalanced.from_t_amplitudes(
+        t2, t1=t1, n_reps=self._n_reps
+    )
+    self._diag_coulomb_mats = ucj_op.diag_coulomb_mats
+    self._orbital_rotations = ucj_op.orbital_rotations
+    self._final_orbital_rotation = ucj_op.final_orbital_rotation
 
   def _initialize_givens_parameters(self):
     for k in range(self._n_reps + 1):
       if k < self._n_reps:
-        rotations, diagonal_phases = ffsim.linalg.givens_decomposition(
-            self._orbital_rotations[k]
-        )
+        mat = self._orbital_rotations[k]
       else:
-        rotations, diagonal_phases = ffsim.linalg.givens_decomposition(
+        mat = (
             self._final_orbital_rotation
+            if self._final_orbital_rotation is not None
+            else np.eye(self._norb, dtype=complex)
         )
+
+      rotations, diagonal_phases = ffsim.linalg.givens_decomposition(mat)
+
       for p, (c, s, i, j) in enumerate(rotations):
-        theta_parameter = Parameter(f"givens_{k}_{p}_theta")
-        beta_parameter = Parameter(f"givens_{k}_{p}_beta")
+        theta_param = Parameter(f"givens_{k}_{p}_theta")
+        beta_param = Parameter(f"givens_{k}_{p}_beta")
 
-        theta = 2 * np.arccos(c)
-        beta = np.angle(s) - np.pi/2
+        r, phi = cmath.polar(s)
+        theta = 2 * math.atan2(r, c)
+        beta = phi - 0.5 * math.pi
 
-        self._orbital_givens_indices_per_layer[k].append(
-            (i, j, theta, beta, theta_parameter, beta_parameter))
+        self._orbital_givens_per_layer[k].append(
+            (i, j, theta, beta, theta_param, beta_param)
+        )
 
       for l, phase in enumerate(diagonal_phases):
-        parameter = Parameter(f"phase_{k}_{l}")
-        phi = np.angle(phase)
-        self._diagonal_phases_indices_per_layer[k].append(
-            (l, phi, parameter))
+        param = Parameter(f"phase_{k}_{l}")
+        phi = cmath.phase(phase)
+        self._diagonal_phases_per_layer[k].append((l, phi, param))
 
   def _initialize_jastrow_parameters(self):
     for k in range(self._n_reps):
+      J_aa = self._diag_coulomb_mats[k, 0]
+      J_ab = self._diag_coulomb_mats[k, 1]
+
       for i in range(self._norb):
         for j in range(i, self._norb):
-          parameter = Parameter(f"jastrow_{k}_{i}_{j}")
-          value = float(self._diag_coulomb_mats[k, i, j])
-          if i == j:
-            value *= 0.5
-          self._jastrow_indices_per_layer[k].append(
-              (i, j, value, parameter))
+          param = Parameter(f"jastrow_aa_{k}_{i}_{j}")
+          value = float(J_aa[i, j])
+          self._jastrow_aa_per_layer[k].append((i, j, value, param))
 
-  def _build_givens_subcircuit(self, layer_idx: int) -> QuantumCircuit:
-    sub = QuantumCircuit(self._norb)
-    self._append_givens_network(sub, layer_idx, qubit_offset=0)
-    return sub
+      for i in range(self._norb):
+        for j in range(self._norb):
+          param = Parameter(f"jastrow_ab_{k}_{i}_{j}")
+          value = float(J_ab[i, j])
+          self._jastrow_ab_per_layer[k].append((i, j, value, param))
 
-  def _append_givens_network(self, circuit: QuantumCircuit, layer_idx: int, qubit_offset: int) -> None:
-    for i, j, _, _, theta_parameter, beta_parameter in self._orbital_givens_indices_per_layer[layer_idx]:
-      circuit.append(XXPlusYYGate(theta_parameter, beta_parameter), [
-          i + qubit_offset, j + qubit_offset])
-    for l, _, parameter in self._diagonal_phases_indices_per_layer[layer_idx]:
-      circuit.append(PhaseGate(parameter), [l + qubit_offset])
+  def _append_givens_network(
+      self, circuit: QuantumCircuit, layer_idx: int, qubit_offset: int
+  ) -> None:
+    for i, j, _, _, theta_p, beta_p in self._orbital_givens_per_layer[layer_idx]:
+      circuit.append(
+          XXPlusYYGate(theta_p, beta_p),
+          [i + qubit_offset, j + qubit_offset],
+      )
+    for l, _, param in self._diagonal_phases_per_layer[layer_idx]:
+      circuit.append(PhaseGate(param), [l + qubit_offset])
 
-  def _append_jastrow(self, circuit: QuantumCircuit, layer_idx: int, qubit_offset: int) -> None:
-    for i, j, _, parameter in self._jastrow_indices_per_layer[layer_idx]:
+  def _append_jastrow(
+      self, circuit: QuantumCircuit, layer_idx: int, qubit_offset: int
+  ) -> None:
+    for i, j, _, param in self._jastrow_aa_per_layer[layer_idx]:
       if i == j:
-        circuit.append(PhaseGate(parameter), [i + qubit_offset])
+        circuit.append(PhaseGate(0.5 * param), [i + qubit_offset])
       else:
-        circuit.append(CPhaseGate(parameter), [
-            i + qubit_offset, j + qubit_offset])
+        circuit.append(
+            CPhaseGate(param), [i + qubit_offset, j + qubit_offset]
+        )
+
+  def _append_jastrow_cross(
+      self,
+      circuit: QuantumCircuit,
+      layer_idx: int,
+      alpha_offset: int,
+      beta_offset: int,
+  ) -> None:
+    for i, j, _, param in self._jastrow_ab_per_layer[layer_idx]:
+      circuit.append(
+          CPhaseGate(param),
+          [i + alpha_offset, j + beta_offset],
+      )
 
   def _register_layer_values(self, layer_idx: int, include_jastrow: bool) -> None:
-    for _, _, theta, beta, theta_parameter, beta_parameter in self._orbital_givens_indices_per_layer[layer_idx]:
-      self._parameters[theta_parameter] = theta
-      self._parameters[beta_parameter] = beta
-    for _, phi, parameter in self._diagonal_phases_indices_per_layer[layer_idx]:
-      self._parameters[parameter] = phi
+    for _, _, theta, beta, theta_p, beta_p in self._orbital_givens_per_layer[layer_idx]:
+      self._parameters[theta_p] = theta
+      self._parameters[beta_p] = beta
+    for _, phi, param in self._diagonal_phases_per_layer[layer_idx]:
+      self._parameters[param] = phi
+
     if include_jastrow:
-      for _, _, value, parameter in self._jastrow_indices_per_layer[layer_idx]:
-        self._parameters[parameter] = value
+      for _, _, value, param in self._jastrow_aa_per_layer[layer_idx]:
+        self._parameters[param] = value
+      for _, _, value, param in self._jastrow_ab_per_layer[layer_idx]:
+        self._parameters[param] = value
 
   def _build_circuit(self) -> QuantumCircuit:
+    if self._diag_coulomb_mats is None or self._orbital_rotations is None:
+      raise RuntimeError(
+          "Call from_t_amplitudes() before building the circuit.")
+
     self._initialize_givens_parameters()
     self._initialize_jastrow_parameters()
     self._parameters = {}
@@ -123,21 +166,26 @@ class UCJ:
     beta = list(range(self._norb, 2 * self._norb))
 
     for k in range(self._n_reps):
-      forward = self._build_givens_subcircuit(k)
+      forward = QuantumCircuit(self._norb)
+      self._append_givens_network(forward, k, qubit_offset=0)
       inverse = forward.inverse()
-
-      circuit.append(forward.to_instruction(), alpha)
-      circuit.append(forward.to_instruction(), beta)
-
-      self._append_jastrow(circuit, k, qubit_offset=0)
-      self._append_jastrow(circuit, k, qubit_offset=self._norb)
 
       circuit.append(inverse.to_instruction(), alpha)
       circuit.append(inverse.to_instruction(), beta)
 
+      self._append_jastrow(circuit, k, qubit_offset=0)
+      self._append_jastrow(circuit, k, qubit_offset=self._norb)
+      self._append_jastrow_cross(
+          circuit, k, alpha_offset=0, beta_offset=self._norb
+      )
+
+      circuit.append(forward.to_instruction(), alpha)
+      circuit.append(forward.to_instruction(), beta)
+
       self._register_layer_values(k, include_jastrow=True)
 
-    final = self._build_givens_subcircuit(self._n_reps)
+    final = QuantumCircuit(self._norb)
+    self._append_givens_network(final, self._n_reps, qubit_offset=0)
     circuit.append(final.to_instruction(), alpha)
     circuit.append(final.to_instruction(), beta)
     self._register_layer_values(self._n_reps, include_jastrow=False)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
+import ffsim
 from qiskit_ibm_runtime import EstimatorV2 as Estimator
 from qiskit_aer import AerSimulator
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
-import ffsim
 
 from molecules.beh2_molecule import BeH2Molecule
 from methods.ccsd import CCSD
@@ -11,56 +11,43 @@ from algorithms.vqe import VQE
 
 def main():
   backend = AerSimulator()
-  target = backend.target
-  pm = generate_preset_pass_manager(target=target, optimization_level=3)
+  pass_manager = generate_preset_pass_manager(
+      target=backend.target, optimization_level=3
+  )
   estimator = Estimator(backend)
 
   mol = BeH2Molecule()
+  num_orbitals = len(mol.get_active_space())
+  n_electrons = mol.get_electrons_number()
 
-  print("=== MOLECULE ===")
-  print("NAO:", mol.get_molecule().nao_nr())
-  print("Active space:", list(mol.get_active_space()))
-  print("Active orbitals:", len(mol.get_active_space()))
-  print("Active electrons:", mol.get_electrons_number())
+  ccsd = CCSD(mol.get_scf(), mol.get_molecule(), mol.get_active_space())
+  ccsd.run()
+  t1, t2 = ccsd.get_amplitudes()
 
-  print("\n=== RHF ===")
-  print("E(RHF):", mol.get_scf().e_tot)
+  ansatz = UCJ(num_orbitals, n_reps=3)
+  ansatz.from_t_amplitudes(t1, t2)
 
-  print("\n=== CASCI ===")
-  print("E(CASCI):", mol.get_casci_energy())
+  circuit = ansatz.get_circuit()
+  parameters = ansatz.get_parameters()
 
-  cc = CCSD(
-      mol.get_scf(),
-      mol.get_molecule(),
-      mol.get_active_space(),
-  )
-  cc.run()
+  initial_state = ffsim.qiskit.PrepareHartreeFockJW(
+      num_orbitals, n_electrons)
 
-  print("\n=== CCSD ===")
-  print("E(CCSD):", cc.get_energy())   # se il tuo wrapper lo espone
-
-  uc = UCJ(len(mol.get_active_space()), 4)
-
-  uc.from_t_amplitudes(
-      cc.get_amplitudes()[0],
-      cc.get_amplitudes()[1],
-  )
-
-  qc = uc.get_circuit()
-  params = uc.get_parameters()
-
-  vq = VQE(
+  vqe = VQE(
       mol.get_qubit_hamiltonian(),
-      qc,
-      params,
+      circuit,
+      parameters,
       estimator,
-      pm,
-      ffsim.qiskit.PrepareHartreeFockJW(
-          len(mol.get_active_space()), mol.get_electrons_number())
+      pass_manager,
+      initial_state,
   )
 
-  print("\n=== VQE ===")
-  print("E(VQE):", vq.run())
+  result = vqe.run()
+
+  print("=== SUMMARY ===")
+  print(f"E(CASCI): {mol.get_casci_energy()}")
+  print(f"E(CCSD):  {ccsd.get_energy()}")
+  print(f"E(VQE):   {result.fun}")
 
 
 if __name__ == "__main__":
